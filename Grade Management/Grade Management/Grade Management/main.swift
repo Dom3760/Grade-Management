444459a62569ca3485608fca62f02a34b0910c26
Dom3760 <142520274+Dom3760@users.noreply.github.com>
2024-02-06 10:30:45 -0800

    Add files via upload

--- Grade Management/Grade Management/Grade Management/main.swift
+++ Grade Management/Grade Management/Grade Management/main.swift
@@ -8,7 +8,7 @@
 import Foundation
 import CSV
 
-var studentName: [String] = []
+var studentNames: [String] = []
 var grade: [[Int]] = []
 
 func startScreen()
@@ -24,14 +24,14 @@ func startScreen()
         let csv = try CSVReader(stream: csvStream)
         
         while let row = csv.next() {
-            studentName.append(row[0])
+            studentNames.append(row[0])
             var studentGrades: [Int] = []
             for i in 1..<row.count {
                 if let grade1 = Int(row[i]) {
                     studentGrades.append(grade1)
-                    print("Student: \(studentName), Grade \(i): \(grade1)")
+//                    print("Student: \(studentNames), Grade \(i): \(grade1)")
                 } else {
-                    print("Error converting grade to integer for \(studentName), Grade \(i)")
+//                    print("Error converting grade to integer for \(studentNames), Grade \(i)")
                 }
             }
             grade.append(studentGrades)
@@ -39,8 +39,6 @@ func startScreen()
     } catch {
         print("Error reading CSV file: \(error)")
     }
-}
-
     
     print ("Welcome, what would you like to do")
     print ("1. Display grade of a single student")
@@ -52,7 +50,7 @@ func startScreen()
     print ("7. Find the highest grade of the class")
     print ("8. Filter students by grade range")
     print ("9. Quit")
-
+    
     if let input1 = readLine()
     {
         if input1 == "1"
@@ -89,55 +87,169 @@ func startScreen()
         }
         if input1 == "9"
         {
+            print("have a good day!")
+            return()
         }else
         {
             print("Error")
             startScreen()
         }
     }
+}
 
-func singleStudentsGrade()
-{
-    print("what students grade do you want to see?")
-    if var input2 = readLine()
-    {
-        
+func singleStudentsGrade() {
+    print (studentNames)
+    print (grade)
+    print("Enter the name of the student:")
+    if let inputName = readLine() {
+        for i in studentNames.indices
+        {
+            if inputName == studentNames[i]
+            {
+                print("\(inputName) found")
+                print("what grade would you like to see (1-10)?")
+                if let input2 = Int(readLine()!)
+                {
+                    print (grade[i][input2 - 1])
+                    sleep(3)
+                    startScreen()
+                }
+            }
+        }
+        print("that name is not available or does not exist.")
+        startScreen()
     }
 }
+
 
 func singleStudentsGrades()
 {
     
+    print("Enter the name of the student:")
+    if let inputName = readLine() {
+        for i in studentNames.indices
+        {
+            if inputName == studentNames[i]
+            {
+                print("\(inputName) found")
+                print("name:\(studentNames[i]) Grades:\(grade[i])")
+                sleep(2)
+                startScreen()
+            }
+        }
+        print("that name is not available or does not exist.")
+        startScreen()
+        
+    }
 }
 
 func allStudentsGrades()
 {
-    
+    print("here are all the grades:")
+    sleep(1)
+    for i in studentNames.indices
+    {
+        print("Name:\(studentNames[i]) Grades: \(grade[i])")
+    }
+    sleep(2)
+    startScreen()
 }
 
 func averageOfClass()
 {
-    
+    var average = 0
+    var currentNum = 0
+    for i in grade.indices
+    {
+        for j in grade.indices
+        {
+            currentNum += grade[i][j]
+        }
+        currentNum = currentNum / grade[i].count
+        average += currentNum
+        currentNum = 0
+        if i == grade.count
+        {
+            average /= grade.count
+            print ("the average is:\(average)")
+            average = 0
+            sleep(1)
+            startScreen()
+        }
+    }
 }
 
 func averageOnAssignment()
 {
-    
+    var current2 = 0
+    print("what assignment would you like to see the average of (1-10)?")
+    if let input1 = Int(readLine()!)
+    {
+        for i in grade.indices
+        {
+            current2 += grade[i][input1]
+        }
+        current2 /= studentNames.count
+        print("the average of that assignement is: \(current2)")
+        current2 = 0
+        sleep(2)
+        startScreen()
+    }
 }
 
 func lowestGrade()
 {
-    
+    var grades2: [Int] = []
+    var current2 = 0
+    for i in studentNames.indices
+    {
+        for j in studentNames.indices
+        {
+            current2 += grade[i][j]
+        }
+        current2 /= studentNames.count
+        grades2.append(current2)
+        current2 = 0
+    }
+    if let lowest = grades2.min(), let index = grades2.firstIndex(of: lowest)
+    {
+        print ("the lowest grade is by: \(studentNames[index]) with a grade of: \(lowest)")
+    }
+//    if let minNumber = numbers.min(), let index = numbers.firstIndex(of: minNumber) {
 }
 
 func highestGrade()
 {
-    
+    var grades2: [Int] = []
+    var current2 = 0
+    for i in studentNames.indices
+    {
+        for j in studentNames.indices
+        {
+            current2 += grade[i][j]
+        }
+        current2 /= studentNames.count
+        grades2.append(current2)
+        current2 = 0
+    }
+    if let highest = grades2.max(), let index = grades2.firstIndex(of: highest)
+    {
+        print ("the highest grade is by: \(studentNames[index]) with a grade of: \(highest)")
+    }
 }
 
 func filter()
 {
-    
+    print("Enter the low range you would like to use?")
+    if let lowRange = readLine()
+    {
+        
+    }
+    print("Enter the high range you would like to use?")
+    if let highRange = readLine()
+    {
+        
+    }
 }
 
 startScreen()
